f46f6bb357249419b8b79209c601784a74eed9ee
Dan Mayer <dan.mayer@shopify.com>
2025-02-19 16:50:13 -0700

    backport some benchmark improvemets and add additional tests showing what is supported for simplified fast get multi

--- test/integration/test_pipelined_get.rb
+++ test/integration/test_pipelined_get.rb
@@ -136,6 +136,126 @@ describe 'Pipelined Get' do
     end
   end
 
+  it 'meta read_multi_req supports pipelined get doesnt default value on misses raw true' do
+    memcached_persistent do |_, port|
+      dc = Dalli::Client.new("localhost:#{port}", namespace: 'some:namspace', raw: true)
+      dc.close
+      dc.flush
+
+      keys_to_query = %w[a b]
+
+      resp = dc.get_multi(keys_to_query)
+
+      assert_empty(resp)
+
+      dc.set('a', 'foo')
+
+      # Invocation without block
+      resp = dc.get_multi(keys_to_query)
+      expected_resp = { 'a' => 'foo' }
+
+      assert_nil(resp['b'])
+      assert_equal(expected_resp, resp)
+
+      # Invocation with block
+      dc.get_multi(keys_to_query) do |k, v|
+        assert(expected_resp.key?(k) && expected_resp[k] == v)
+        expected_resp.delete(k)
+      end
+
+      assert_empty expected_resp
+    end
+  end
+
+  it 'supports pipelined get features for read_multi_req' do
+    memcached_persistent do |_, port|
+      dc = Dalli::Client.new("localhost:#{port}", namespace: 'some:namspace')
+      dc.close
+      dc.flush
+      resp = dc.get_multi(%w[a b c d e f])
+
+      assert_empty(resp)
+
+      dc.set('a', 'foo')
+      dc.set('b', 123)
+      dc.set('c', %w[a b c])
+
+      # Invocation without block
+      resp = dc.get_multi(%w[a b c d e f])
+      expected_resp = { 'a' => 'foo', 'b' => 123, 'c' => %w[a b c] }
+
+      assert_equal(expected_resp, resp)
+
+      # Invocation with block
+      dc.get_multi(%w[a b c d e f]) do |k, v|
+        assert(expected_resp.key?(k) && expected_resp[k] == v)
+        expected_resp.delete(k)
+      end
+
+      assert_empty expected_resp
+
+      # Perform a big quiet set with 1000 elements.
+      arr = []
+      dc.multi do
+        1000.times do |idx|
+          dc.set idx, idx
+          arr << idx
+        end
+      end
+
+      # Retrieve the elements with a pipelined get
+      result = dc.get_multi(arr)
+
+      assert_equal(1000, result.size)
+      assert_equal(50, result['50'])
+    end
+  end
+
+  it 'supports pipelined get features for read_multi_req when raw is true' do
+    memcached_persistent do |_, port|
+      dc = Dalli::Client.new("localhost:#{port}", namespace: 'some:namspace', raw: true)
+      dc.close
+      dc.flush
+      resp = dc.get_multi(%w[a b c d e f])
+
+      assert_empty(resp)
+
+      # raw is true mode only supports string values
+      dc.set('a', 'foo')
+      dc.set('b', '123')
+      dc.set('c', "a\r\nb\r\nc")
+
+      # Invocation without block
+      resp = dc.get_multi(%w[a b c d e f])
+      expected_resp = { 'a' => 'foo', 'b' => '123', 'c' => "a\r\nb\r\nc" }
+
+      assert_equal(expected_resp, resp)
+
+      # Invocation with block
+      dc.get_multi(%w[a b c d e f]) do |k, v|
+        assert(expected_resp.key?(k) && expected_resp[k] == v)
+        expected_resp.delete(k)
+      end
+
+      assert_empty expected_resp
+
+      # Perform a big quiet set with 1000 elements.
+      arr = []
+      dc.multi do
+        1000.times do |idx|
+          dc.set idx, idx.to_s
+          arr << idx
+        end
+      end
+
+      # Retrieve the elements with a pipelined get
+      result = dc.get_multi(arr)
+
+      assert_equal(1000, result.size)
+      assert_equal('50', result['50'])
+    end
+  end
+
   it 'raises network errors' do
     toxi_memcached_persistent(19_997, '', { down_retry_delay: 0 }) do |dc|
       dc.close
@@ -179,6 +299,44 @@ describe 'Pipelined Get' do
       # Invocation without block
       resp = dc.get_multi(keys_to_query)
       expected_resp = { 'a' => 'foo', 'contains space' => 123, 'ƒ©åÍÎ' => %w[a b c] }
+
+      assert_equal(expected_resp, resp)
+
+      # Invocation with block
+      dc.get_multi(keys_to_query) do |k, v|
+        assert(expected_resp.key?(k) && expected_resp[k] == v)
+        expected_resp.delete(k)
+      end
+
+      assert_empty expected_resp
+    end
+  end
+
+  it 'supports pipelined get with keys does not support Unicode or spaces for read_multi_req with raw true' do
+    memcached_persistent do |_, port|
+      # NOTE: the meta protocol does not allow spaces in keys
+      # > the key must not include control characters or whitespace.
+      # https://github.com/memcached/memcached/blob/master/doc/protocol.txt
+      # unicode support in meta protocol is unclear, but seems best to not support
+      # see similar discussion in pymemcachehttps://github.com/pinterest/pymemcache/issues/122
+      # current optimized raw version can fail on unicode which could not pass the simplified byte parser
+      dc = Dalli::Client.new("localhost:#{port}", namespace: 'some:namspace', raw: true)
+      dc.close
+      dc.flush
+
+      keys_to_query = %w[a b contains_no_space normal_key]
+
+      resp = dc.get_multi(keys_to_query)
+
+      assert_empty(resp)
+
+      dc.set('a', 'foo')
+      dc.set('contains_no_space', '123')
+      dc.set('normal_key', "a\r\nb\r\nc")
+
+      # Invocation without block
+      resp = dc.get_multi(keys_to_query)
+      expected_resp = { 'a' => 'foo', 'contains_no_space' => '123', 'normal_key' => "a\r\nb\r\nc" }
 
       assert_equal(expected_resp, resp)
 
